c0b68bed71edf0c763fea12f147916f7d6b6ddae
Sune Rønne <SURN@nykredit.dk>
2024-09-29 23:44:30 +0200

    For today

---
+++ SuneDoes.UI/Pages/OnlineDating/OnlineDatingAppComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SuneDoes.UI.Pages.OnlineDating;
+
+public partial class OnlineDatingAppComponent
+{
+    [Parameter]
+    public string Title { get; set; }
+
+    [Parameter]
+    public RenderFragment Text { get; set; }
+
+    [Parameter]
+    public RenderFragment AppLogo { get; set; }
+
+
+}

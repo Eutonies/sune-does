97869da4a2909f799ec7a1f301f8afc99be7e935
Sune Rønne <SURN@nykredit.dk>
2024-09-30 22:45:05 +0200

    For today!

--- SuneDoes.UI/Pages/OnlineDating/OnlineDatingPage.razor.cs
+++ SuneDoes.UI/Pages/OnlineDating/OnlineDatingPage.razor.cs
@@ -1,8 +1,16 @@
 ﻿
+using Microsoft.AspNetCore.Components;
+using SuneDoes.UI.Components;
+using SuneDoes.UI.Session;
+
 namespace SuneDoes.UI.Pages.OnlineDating;
 
 public partial class OnlineDatingPage
 {
+    private static ImageShowComponent.ShowImage Image(string app, string imageName, string name) =>
+        new($"images/online-dating/{app}/{imageName}", name);
+    private static ImageShowComponent.ShowImage TinderImage(string imageName, string name) => Image("tinder", imageName, name);
+
     protected override Task OnParametersSetAsync()
     {
         if(SessionState!= null && SessionState.SelectedPage != Session.SessionSelectedPage.OnlineDating)
@@ -10,6 +18,20 @@ public partial class OnlineDatingPage
             SessionState.SelectedPage = Session.SessionSelectedPage.OnlineDating;
         }
         return base.OnParametersSetAsync();
+    }
+
+    protected override Task OnInitializedAsync()
+    {
+        if(SessionState.CurrentShowImages == null)
+        {
+            SessionState.ShowImages("Online Dating Images",
+                TinderImage("tinder-frontpage.png", "Adventure"),
+                TinderImage("sune-w-quote-1.png", "Jesper endorsement 1/3"),
+                TinderImage("sune-w-quote-2.png", "Jesper endorsement 2/3"),
+                TinderImage("sune-w-quote-3.png", "Jesper endorsement 3/3")
+            );
+        }
+        return Task.CompletedTask;
     }
 
 }
--- SuneDoes.UI/Session/SessionState.cs
+++ SuneDoes.UI/Session/SessionState.cs
@@ -1,4 +1,6 @@
-﻿namespace SuneDoes.UI.Session;
+﻿using SuneDoes.UI.Components;
+
+namespace SuneDoes.UI.Session;
 
 public record SessionState(Action OnUpdate)
 {
@@ -14,6 +16,24 @@ public record SessionState(Action OnUpdate)
             }
         } }
 
+    private string? _currentShowImagesTitle;
+    public string? CurrentShowImagesTitle => _currentShowImagesTitle;
+
+    private IReadOnlyCollection<ImageShowComponent.ShowImage>? _currentShowImages;
+    public IReadOnlyCollection<ImageShowComponent.ShowImage>? CurrentShowImages => 
+        _currentShowImages;
+    public void ShowImages(string curremtShowImagesTitle, params ImageShowComponent.ShowImage[] images)
+    {
+        _currentShowImagesTitle = curremtShowImagesTitle;
+        _currentShowImages = images;
+        OnUpdate();
+    }
+    public void StopShowImages()
+    {
+        _currentShowImagesTitle = null;
+        _currentShowImages = null;
+        OnUpdate();
+    }
 
     public event EventHandler<SessionSelectedPage?> CurrentSelectedPageChanged;
 

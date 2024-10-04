3312bb4f092d7b94f9169509f3550b0201a2c0ae
Sune Rønne <SURN@nykredit.dk>
2024-10-04 16:37:31 +0200

    Meditation tracks pimping

--- SuneDoes.UI/Pages/Meditation/MeditationTracksComponent.razor.cs
+++ SuneDoes.UI/Pages/Meditation/MeditationTracksComponent.razor.cs
@@ -1,12 +1,19 @@
-﻿namespace SuneDoes.UI.Pages.Meditation;
+﻿using Microsoft.AspNetCore.Components;
+using SuneDoes.Extensions;
+
+namespace SuneDoes.UI.Pages.Meditation;
 
 public partial class MeditationTracksComponent
 {
+
+    private static string ButtonImage(int no) => $"images/meditation/button-image-0{no}.webp";
+
     private static readonly IReadOnlyCollection<MeditationTrack> MeditationTracks = [
         new (
             TrackId: "1618123647", 
             TrackName: "The Garden", 
             TrackUrl: "https://soundcloud.com/sune-tzu-forkbeard/meditations-chapter01-the-garden", 
+            ButtonImage: ButtonImage(1),
             Summary: [
                 "You are introduced to the fundamental breathing techniques: Zen-breathing, Shotgun-breathing, and Uzi-breathing.",
                 "Your first exploration of your happy place is disturbed by a filthy presence."
@@ -15,6 +22,7 @@ public partial class MeditationTracksComponent
             TrackId: "1621832136",
             TrackName: "The Forest",
             TrackUrl: "https://soundcloud.com/sune-tzu-forkbeard/meditations-chapter02-the-forest",
+            ButtonImage: ButtonImage(2),
             Summary: [
                 "You journey deeper into the depths of your consciousness and have a first class chance to meditate on the boundary between inter-human trust and self-sufficiency."
                 ]),
@@ -22,6 +30,7 @@ public partial class MeditationTracksComponent
             TrackId: "1627914372",
             TrackName: "The Tavern",
             TrackUrl: "https://soundcloud.com/sune-tzu-forkbeard/chapter03-the-tavern",
+            ButtonImage: ButtonImage(3),
             Summary: [
                 "From confronting the deep and hidden workings of your mind, you are thrust into meditations on the reflections of the outside world on your mind.",
                 "And then you dance.",
@@ -31,6 +40,7 @@ public partial class MeditationTracksComponent
             TrackId: "1633424091",
             TrackName: "The High-rise",
             TrackUrl: "https://soundcloud.com/sune-tzu-forkbeard/chapter04-thehighrise",
+            ButtonImage: ButtonImage(4),
             Summary: [
                 "Sharpening your mind and honing your conscious thought allows you to be a productive and influencial part of society, but what to do with that productivity and what parts of society to influence?",
                 "You have a chance to sky-rocket into the filthy rich 0.1%, but is it really all it's cracked up to be?",
@@ -40,6 +50,7 @@ public partial class MeditationTracksComponent
             TrackId: "1637704026",
             TrackName: "The Storm",
             TrackUrl: "https://soundcloud.com/sune-tzu-forkbeard/chapter05-thestorm",
+            ButtonImage: ButtonImage(5),
             Summary: [
                 "Some men cower or run in the face of a storm, while others will hold their ground. How will you deal with the inevitable Storm of Change?",
                 "And there is glass!"
@@ -48,6 +59,7 @@ public partial class MeditationTracksComponent
             TrackId: "1650975804",
             TrackName: "The Slaughterhouse",
             TrackUrl: "https://soundcloud.com/sune-tzu-forkbeard/chapter06-theslaughterhouse",
+            ButtonImage: ButtonImage(6),
             Summary: [
                 "Some men will run straight into the storm, while others yet bring the storm.",
                 "How will you fight pure evil in the storm-ridden wonderland of your mind?"
@@ -56,12 +68,19 @@ public partial class MeditationTracksComponent
         ];
 
 
+    private static MarkupString ButtonImageCssRules => MeditationTracks
+        .Select((_, indx) => indx + 1)
+        .Select(indx => $"#sundo-meditation-accordian-button-{indx} {{ background-image: url('images/meditation/button-image-0{indx}.webp);}}")
+        .MakeString("\r\n")
+        .Pipe(_ => new MarkupString(_));
+
 
 
     public record MeditationTrack(
         string TrackId,
         string TrackName,
         string TrackUrl,
+        string ButtonImage,
         IReadOnlyCollection<string> Summary
         );
 

d3bef9fd733882c76ba265b14d0fb4448a069ec2
Sune Rønne <SURN@nykredit.dk>
2024-10-07 23:48:33 +0200

    Fo today

---
+++ SuneDoes.UI/Pages/Home/HomeMeditationComponent.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using SuneDoes.UI.Pages.Meditation;
+using SuneDoes.UI.Pages.OnlineDating;
+using System.Reflection;
+
+namespace SuneDoes.UI.Pages.Home;
+
+public partial class HomeMeditationComponent
+{
+    [Inject]
+    public NavigationManager NavManager { get; set; }
+
+    private void OnImageClick(MouseEventArgs ev)
+    {
+        var url = typeof(MeditationPage)
+            .GetCustomAttribute<RouteAttribute>()!
+            .Template;
+        NavManager.NavigateTo(url);
+    }
+
+}

89d288b39f3855d3cec0432018a9b7ab8733e2fb
Sune Roenne <sune_roenne@outlook.com>
2024-10-13 19:07:46 +0200

    Some last mods

--- SuneDoes.UI/sunedoes.portforward.sh
+++ SuneDoes.UI/sunedoes.portforward.sh
@@ -1,1 +1,8 @@
 kubectl port-forward service/sunedoes 80:80 --address=0.0.0.0
+
+Ctrl+Z (put process on hold)
+bg (run process in background)
+
+jobs -l (list jobs)
+fg <job-number> (bring process to foreground)
+Ctr+C

c6d0a0ca22a5ec444dc1463bcff47d6bce184780
Sune Rønne <SURN@nykredit.dk>
2024-10-16 18:13:18 +0200

    Perty good

---
+++ SuneDoes.UI/Pages/Home/HomeWritingComponent.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SuneDoes.UI.Pages.Home;
+
+public partial class HomeWritingComponent
+{
+    [Parameter]
+    public string Title { get; set; }
+
+    [Parameter]
+    public RenderFragment BodyFragment { get; set; }
+
+    [Parameter]
+    public RenderFragment FooterFragment { get; set; }
+
+}
---
+++ SuneDoes.UI/Pages/Home/HomeWritingsComponent.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using SuneDoes.UI.Layout.Main;
+
+namespace SuneDoes.UI.Pages.Home;
+
+public partial class HomeWritingsComponent
+{
+    [CascadingParameter]
+    public FileDownloader FileDownloader { get; set; }
+
+    private void OnJohnAndAngelaPdfDownloadClicked(MouseEventArgs ev)
+    {
+        _ = FileDownloader.DownloadFileFromUrl("Angela and John - Forever.pdf", "docs/Angela-and-John-Forever.pdf");
+    }
+    private void OnSimrebogenPdfClicked(MouseEventArgs ev)
+    {
+        _ = FileDownloader.DownloadFileFromUrl("Simrebogen.pdf", "docs/Simrebogen.pdf");
+    }
+
+}

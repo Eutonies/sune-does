c78a8614193cc2a0d9846a580a6b694cc5166cba
Sune Rønne <SURN@nykredit.dk>
2024-10-19 00:22:17 +0200

    Checkah checkah

---
+++ SuneDoes.UI/Pages/LucidDreaming/LucidDreamComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SuneDoes.UI.Pages.LucidDreaming;
+
+public partial class LucidDreamComponent
+{
+    [Parameter]
+    public DateTime DreamDate { get; set; }
+
+    [Parameter]
+    public RenderFragment TextPart { get; set; }
+
+
+}
--- SuneDoes.UI/Pages/LucidDreaming/LucidDreamingPage.razor.cs
+++ SuneDoes.UI/Pages/LucidDreaming/LucidDreamingPage.razor.cs
@@ -2,11 +2,13 @@
 using Microsoft.AspNetCore.Components;
 using SuneDoes.UI.Components;
 using SuneDoes.UI.Session;
+using System.Globalization;
 
 namespace SuneDoes.UI.Pages.LucidDreaming;
 
 public partial class LucidDreamingPage
 {
+    private readonly CultureInfo _daDk = CultureInfo.CreateSpecificCulture("da-DK");
     protected override Task OnParametersSetAsync()
     {
         if(SessionState!= null)
@@ -24,6 +26,8 @@ public partial class LucidDreamingPage
         }
         return Task.CompletedTask;
     }
+
+    private DateTime D(string dateString) => DateTime.ParseExact(dateString, "dd-MM-yyyy", _daDk);
 
 
 }

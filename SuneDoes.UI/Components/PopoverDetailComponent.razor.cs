49eaf36f9a64c9b293d1e10b31aa667000f1223a
Sune Rønne <SURN@nykredit.dk>
2024-10-20 01:24:15 +0200

    I so cool

---
+++ SuneDoes.UI/Components/PopoverDetailComponent.razor.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text;
+
+namespace SuneDoes.UI.Components;
+
+public partial class PopoverDetailComponent
+{
+    [Parameter]
+    public string? TextColor { get; set; }
+
+    [Parameter]
+    public string? BorderColor { get; set; }
+
+    [Parameter]
+    public string? ButtonBackgroundColor { get; set; }
+
+    [Parameter]
+    public string? PopoverBackgroundColor { get; set; }
+
+
+    [Parameter]
+    public string? FontFamily { get; set; }
+
+    [Parameter]
+    public int? FontSize { get; set; }
+
+    [Parameter]
+    public string ButtonText { get; set; }
+
+    [Parameter]
+    public string PopoverText { get; set; }
+
+
+    private static long _currentId = 0;
+    private static object _idLock = new object();
+    private static long NextId()
+    {
+        lock (_idLock)
+            return ++_currentId;
+    }
+
+    private readonly long _id = NextId();
+    private string _stringId => $"sundo-popover-detailed-{_id}";
+
+
+
+    private string BuildButtonStyleAttribute()
+    {
+        var returnee = new StringBuilder();
+        returnee.Append(BuildGeneralStyleAttribute());
+        if (ButtonBackgroundColor != null)
+            returnee.Append($"background-color: {ButtonBackgroundColor}; ");
+        return returnee.ToString();
+    }
+
+    private string BuildPopoverStyleAttribute()
+    {
+        var returnee = new StringBuilder();
+        returnee.Append(BuildGeneralStyleAttribute());
+        if (PopoverBackgroundColor != null)
+            returnee.Append($"background-color: {PopoverBackgroundColor}; ");
+        return returnee.ToString();
+    }
+
+
+    private StringBuilder BuildGeneralStyleAttribute()
+    {
+        var returnee = new StringBuilder();
+        if (TextColor != null)
+            returnee.Append($"color: {TextColor}; ");
+        if (BorderColor != null)
+            returnee.Append($"border-color: {BorderColor}; ");
+        if (FontFamily != null)
+            returnee.Append($"font-family: {FontFamily}; ");
+        if (FontSize != null)
+            returnee.Append($"font-size: {FontSize}; ");
+        return returnee;
+
+    }
+
+
+}
--- SuneDoes.UI/Pages/LucidDreaming/LucidDreamingPage.razor.cs
+++ SuneDoes.UI/Pages/LucidDreaming/LucidDreamingPage.razor.cs
@@ -77,4 +77,43 @@ public partial class LucidDreamingPage
 
     private record Glimmer(int PercentLeft, int AnimationTimeSeconds, int InitialDelay);
 
+    private const string Det0201YoungWoman = @"She is actually referred to as 'a young woman' and the term is hence not used to anonymize anyone here.";
+
+    private const string Det0201RomanceEnsures = @"It only seems fair to clarify, that the 'romance' in question was between myself and the young woman. If I am not mistaken, we tried to complete sexual intercourse 
+in a queen-size bed mounted on 4 100m wooden poles that gave sway to our every movement. Our mating-attempt was unsuccesful.";
+
+    private const string Det0601Lotte = @"The name 'Lotte' actually appears here which strikes me as weird, since I don't think I have ever had an even remotely close relation to anyone of that name.";
+
+    private const string Det0801TAP = @"I have googled it, and I don't think the local shopping mall: 'Ballerup Centret' has ever had a restaurant of this name";
+
+    private const string Det0801HerlevKoll = @"Both she and I have had residency in the dorm: 'Herlev Kollegiet', so this part may not be as random as it appears at first. The fact that bus route 147 
+doesn't come anywhere near Herlev Kollegiet can easily be chalked up to me never using public transportation. 
+";
+
+    private const string Det1001Jacq = @"Pseudonym for a girl I went to high school with.";
+
+    private const string Det1001MouhGoon = @"Translates to something like 'Martial Arts Hall', and refers to the place where I had been practicing South Chinese Kong Fu for about 6 years at the time of the dream.";
+
+    private const string Det1701SingleLiving = @"There are pro's and con's to single-living. Not having to worry that you've punched someone while aggresively throwing fists across the bed is a definite pro!";
+
+    private const string Det3001AngryXmas = @"From the way I have it written down, it's unclear to me if I was angry at the rivalling gangsters for intruding on our family Christmas celebration,
+or if my mood was ruined by their lack of social grace in leaving before the main dish was served. If pressed, I would have to go with the former but I'm really not sure.";
+
+    private const string Det0202Soccer = @"On a daily basis, I dedicate about 0.0% of my mental processes on soccer/football, and I really can't give any reasonable explanation for why soccer/football 
+matches are part of the scenery of such a relatively large quantity of my dreams during this period.";
+}
+
+
+public class LucidPopover : PopoverDetailComponent
+{
+    public LucidPopover()
+    {
+        ButtonBackgroundColor = "rgba(250, 240, 240,0.6)";
+        PopoverBackgroundColor = "rgba(250, 230, 230,0.9)";
+        TextColor = "rgb(40, 14, 14)";
+        FontFamily = "Merienda";
+        FontSize = 12;
+        BorderColor = "rgba(160, 61, 61,0.6)";
+
+    }
 }

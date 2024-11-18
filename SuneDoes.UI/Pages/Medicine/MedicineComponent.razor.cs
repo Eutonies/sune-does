16dea652afa129ec6fbb8a7f8e60b9157f5fdc0f
Sune Rønne <SURN@nykredit.dk>
2024-11-18 16:49:55 +0100

    Go on

---
+++ SuneDoes.UI/Pages/Medicine/MedicineComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace SuneDoes.UI.Pages.Medicine;
+
+public partial class MedicineComponent
+{
+    [Parameter]
+    public string MedicineName { get; set; }
+
+    [Parameter]
+    public RenderFragment MedicineDescription { get; set; }
+
+    [Parameter]
+    public RenderFragment Suggestion { get; set; }
+
+
+}
---
+++ SuneDoes.UI/Pages/Medicine/MedicinePage.razor.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using SuneDoes.UI.Components;
+using SuneDoes.UI.Session;
+using System.Globalization;
+
+namespace SuneDoes.UI.Pages.Medicine;
+
+public partial class MedicinePage
+{
+  
+
+    protected override async Task OnParametersSetAsync()
+    {
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+    }
+
+}
+
+

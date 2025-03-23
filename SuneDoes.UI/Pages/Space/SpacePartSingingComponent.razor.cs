9ec58d13ab62329453bca7385789c92916663ad7
Sune Rønne <SURN@nykredit.dk>
2025-03-23 12:04:53 +0100

    Fo right now!

---
+++ SuneDoes.UI/Pages/Space/SpacePartSingingComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿using Booktex.Domain.Book.Model;
+using Booktex.Domain.Util;
+using Microsoft.AspNetCore.Components;
+
+namespace SuneDoes.UI.Pages.Space;
+
+public partial class SpacePartSingingComponent
+{
+    [Parameter]
+    public BookSinging Singing { get; set; }
+
+    private IReadOnlyCollection<(string Line, int Index)> AllLines => Singing
+        .LinesSong
+        .Select((_,indx) => (Line: _, Index: indx))
+        .ToReadonlyCollection(); 
+
+}
